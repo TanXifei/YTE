c5839dfb463d6ba5520a465387b6b652d5d19dd3
QBYQ <q15118029270@163.com>
2026-03-04 01:08:59 -0500

    Fixed Hitachi VIB 181A Button font issue.

--- fabric/src/main/java/top/xfunny/mod/client/render/RenderHitachiVIB181AButton.java
+++ fabric/src/main/java/top/xfunny/mod/client/render/RenderHitachiVIB181AButton.java
@@ -195,7 +195,7 @@ public class RenderHitachiVIB181AButton extends BlockEntityRenderer<HitachiVIB18
                 liftFloorDisplayView.setBasicsAttributes(world,
                         blockPos,
                         sortedPositionsAndLifts.get(i).right(),
-                        FontList.instance.getFont("hhitachi-bxsclc5"),
+                        FontList.instance.getFont("hitachi-bxsclc5"),
                         5F,
                         0xFFFF4800);
                 liftFloorDisplayView.setTextureId(String.format("hitachi_vib_181A_dot_matrix_display_%d", i));
